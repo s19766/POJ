43c33184297d7fe1f7c7bcb332a6c7a7954f6bbd
s19766 <48855467+s19766@users.noreply.github.com>
2019-04-03 14:49:40 +0200

    Update zadanie2_230319.java

--- zadanie2_230319.java
+++ zadanie2_230319.java
@@ -7,19 +7,26 @@ Klasa powinna posiadać metody obliczające pole, obwód i długość przekątne
 */
 
 
-public class prostokat {
+public class Prostokat {
+    
+    private int x, y;
+    
+    public Prostokat(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 
-    static int getPole(int x, int y) {
+    public double getPole() {
         int pole=x*y;
         return pole; //pole prostokata
     }
     
-    static int getObwod(int x, int y) {
+    public int getObwod() {
         int obwod=(2*x)+(2*y);
         return obwod; //obwod prostokata
     }
     
-    static double getPrzekatna(int x, int y) {
+    public double getPrzekatna() {
         double d = Math.pow(x,2)+Math.pow(y,2); //dlugosc przekatnej prostokata
         double sqrt = Math.sqrt(d);
         return sqrt;
@@ -27,12 +34,11 @@ public class prostokat {
     
     
     public static void main(String args[]) {
-        int x = 5; //dlugosc
-        int y = 10; //szerokosc
+        Prostokat prostokat = new Prostokat(5, 10);
         
-        System.out.println("Pole prostokata o wartosciach x, y rowna sie = " + getPole(x, y));
-        System.out.println("Obwod prostokata o wartosciach x, y rowna sie = " + getObwod(x, y));
-        System.out.println("Dlugosc przekatnej prostokata o wartosciach x, y to = " + getPrzekatna(x, y));
+        System.out.println("Pole prostokata o wartosciach x, y rowna sie = " + prostokat.getPole());
+        System.out.println("Obwod prostokata o wartosciach x, y rowna sie = " + prostokat.getObwod());
+        System.out.println("Dlugosc przekatnej prostokata o wartosciach x, y to = " + prostokat.getPrzekatna());
     }
     
 }
