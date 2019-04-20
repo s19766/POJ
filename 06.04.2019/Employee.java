feb49fc372a92c8804d0af2fec5cd7d423ba2cde
s19766 <48855467+s19766@users.noreply.github.com>
2019-04-20 10:34:03 +0200

    Add fix to not working salary

--- 06.04.2019/Employee.java
+++ 06.04.2019/Employee.java
@@ -13,65 +13,55 @@ public class Employee {
     }
 
     public int getID() {
-        System.out.println("Podaj id uzytkownika: ");
-        Scanner ID = new Scanner(System.in);
-        id = ID.nextInt();
         return id;
     }
 
     public String getFirstName() {
-        System.out.println("Podaj imie uzytkownika: ");
-        Scanner FN = new Scanner(System.in);
-        firstName = FN.next();
         return firstName;
     }
 
     public String getLastName() {
-        System.out.println("Podaj nazwisko uzytkownika: ");
-        Scanner LN = new Scanner(System.in);
-        lastName = LN.next();
         return lastName;
     }
 
     public String getName() {
-        String name = firstName + lastName;
-        return name;
+        return firstName + " " + lastName;
     }
 
     public int getSalary() {
-        System.out.println("Podaj pensje uzytkownika: ");
-        Scanner S = new Scanner(System.in);
-        salary = S.nextInt();
         return salary;
     }
 
-    public void setSalary(int salary) {
-        this.salary = salary;
+    public void setSalary(int newSalary) {
+        salary = newSalary;
     }
 
     //roczna pensja
     public int getAnnualSalary() {
-        salary = super(int salary) * 12;
-        return salary;
+        return salary * 12;
     }
 
     public int raiseSalary(int percent) {
-        percent = 2;
-        salary = getAnnualSalary() * percent;
+        salary += (int)(salary*(percent/100.0f));
         return salary;
     }
 
     @Override
     public String toString() {
-        return "Employee[id = " + id + ", name = " + getName() + ", salary = " + raiseSalary(salary) + "]";
+        return "Employee[id = " + id + ", name = " + getName() + ", salary = " + salary + "]";
     }
     
     public static void main(String[] args) {
-        Employee employee = new Employee(19766, 3000, "Damian", "Eggert");
+        Employee employee = new Employee(19766, 0, "Damian", "Eggert");
         System.out.println(employee.getID());
         System.out.println(employee.getFirstName());
         System.out.println(employee.getLastName());
+        System.out.println(employee.getName());
         System.out.println(employee.getSalary());
-        System.out.println(employee.toString());
+        employee.setSalary(5000);
+        System.out.println(employee.getSalary());
+        System.out.println(employee.getAnnualSalary());
+        employee.raiseSalary(20);
+        System.out.println(employee.getSalary());
     }
 }
