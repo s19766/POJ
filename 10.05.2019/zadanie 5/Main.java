caeb5fad6546f8a42f5a4660bb75188b00a35345
s19766 <48855467+s19766@users.noreply.github.com>
2019-05-23 22:11:19 +0200

    Update Main.java

--- 10.05.2019/zadanie 5/Main.java
+++ 10.05.2019/zadanie 5/Main.java
@@ -1,3 +1,9 @@
+// Praca w parach: Adrian Szostak, Damian Eggert
+
+/*
+2.7  Ex: The Customer and Invoice classes
+Source: http://www.ntu.edu.sg/home/ehchua/programming/java/J3f_OOPExercises.html#zz-2.7
+*/
 public class Main {
 
     public static void main(String[] args){
