f8e09c804ac56010bcccbc3ed054c2606cd531ef
s19766 <48855467+s19766@users.noreply.github.com>
2019-05-24 13:42:16 +0200

    Update Shape.java

--- 27.04.2019/zadanie 4/Shape.java
+++ 27.04.2019/zadanie 4/Shape.java
@@ -1,3 +1,9 @@
+// Praca: Damian Eggert
+
+/*
+4.5  Ex: Superclass Shape and its subclasses Circle, Rectangle and Square
+Source: http://www.ntu.edu.sg/home/ehchua/programming/java/J3f_OOPExercises.html
+*/
 public class Shape {
 
     private String color = "red";
