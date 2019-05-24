0bd8abbd4b21c47fbdac81bc13ea538703babbff
s19766 <48855467+s19766@users.noreply.github.com>
2019-05-24 13:43:23 +0200

    Update Main.java

--- 27.04.2019/zadanie 5/Main.java
+++ 27.04.2019/zadanie 5/Main.java
@@ -1,3 +1,14 @@
+// Praca: Damian Eggert
+
+/*
+Write class to check if a number is Armstrong number or
+not? (input 153 output true, 123 output false)
+An Armstrong number of 3 digit is a number for which sum of
+cube of its digits are equal to number e.g. 371 is an Armstrong
+number because 3*3*3 + 7*7*7 + 1*1*1 = 371).
+
+Source: http://www.java67.com/2013/01/10-programming-questions-and-exercises.html
+*/
 public class Main {
     public static void main(String[] args) {
         int number = 153;
