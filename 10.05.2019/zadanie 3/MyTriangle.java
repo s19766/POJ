5d216a5e7b18b592c27487368dda0fa40987ce6a
s19766 <48855467+s19766@users.noreply.github.com>
2019-05-25 00:16:52 +0200

    Create MyTriangle.java

---
+++ 10.05.2019/zadanie 3/MyTriangle.java
@@ -0,0 +1,77 @@
+/*
+POJ, lab5, zadanie 2
+Adrian Szostak, s19777
+Damian Eggert, s19766
+*/
+
+import static java.lang.Math.pow;
+import static java.lang.StrictMath.sqrt;
+
+public class MyTriangle extends  MyPoint {
+
+    int x1,x2,x3,y1,y2,y3;
+    String type;
+    double bokA=sqrt(pow((x1-x2),2)+(pow((y1-y2),2)));
+    double bokB=sqrt(pow((x2-x3),2)+(pow((y2-y3),2)));
+    double bokC=sqrt(pow((x3-x1),2)+(pow((y3-y1),2)));
+
+    public MyTriangle(int x1,int y1,int x2,int y2,int x3,int y3){
+        this.x1=x1;
+        this.y1=y1;
+        this.x2=x2;
+        this.y2=y2;
+        this.x3=x3;
+        this.y3=y3;
+    }
+
+    public void v1(int x1, int y1){
+        this.x1=x1;
+        this.y1=y1;
+        System.out.println("x1: "+x1+" y1:"+y1);
+    }
+
+    public void v2(int x2, int y2){
+        this.x2=x2;
+        this.y2=y2;
+        System.out.println("x2: "+x2+" y2:"+y2);
+    }
+
+    public void v3(int x3, int y3){
+        this.x3=x3;
+        this.y3=y3;
+        System.out.println("x3: "+x3+" y3:"+y3);
+    }
+
+    public String tooString(){
+        return "[v1="+x1+" , "+y1+"v2="+x2+" , "+y2+",v3= "+x3+" , "+y3+" ]";
+    }
+
+    double getPerimeter(int x, int y, int x2, int y2){
+        double distance;
+        distance=sqrt(pow((x-x2),2)+(pow((y-y2),2)));
+        return distance;
+    }
+
+    String getType(String type){
+        if(bokA==bokB&&bokA==bokC&&bokB==bokC){
+            type="Equilateral"; // Rownoboczny
+        }
+        if(bokA==bokB||bokA==bokC||bokB==bokC){
+            type="Isosceles"; // Rownoramienny
+        }
+        if(bokA!=bokB&&bokA!=bokC&&bokB!=bokC){
+            type="Scalene"; // Różnoboczny
+        }
+        return " ";
+    }
+
+
+
+    public static void main(String[] args) {
+        MyPoint punkt = new MyPoint();
+        MyTriangle triangle = new MyTriangle(1,1,2,2,3,3);
+        triangle.tooString();
+
+
+    }
+}
