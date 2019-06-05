9f9c8c61d1ec0b8132b24bf67fa444f972ec0908
s19766 <48855467+s19766@users.noreply.github.com>
2019-06-06 00:05:25 +0200

    Create Point.java

---
+++ 25.05.2019/zadanie 1/Point.java
@@ -0,0 +1,41 @@
+/**
+ * @author Rafał Sochacki s20047
+ * @author Damian Egert s19766
+ */
+public class Point {
+    private int x = 0;
+    private int y = 0;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point() {
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int[] getXY() {
+        return new int[]{x, y};
+    }
+
+    public void setXY(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
