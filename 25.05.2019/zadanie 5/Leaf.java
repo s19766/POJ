b9da0f00e0c6c33a7b51a89c282310d45727945e
s19766 <48855467+s19766@users.noreply.github.com>
2019-06-06 00:16:49 +0200

    Create Leaf.java

---
+++ 25.05.2019/zadanie 5/Leaf.java
@@ -0,0 +1,10 @@
+/**
+ * @author Rafał Sochacki s20047
+ * @author Damian Egert s19766
+ */
+public class Leaf implements Turner {
+    @Override
+    public void turn() {
+        System.out.println("Changing colors");
+    }
+}
