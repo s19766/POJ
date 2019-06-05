f374432bcbcf57a9e379fecc5ee776ef4be8274e
s19766 <48855467+s19766@users.noreply.github.com>
2019-06-06 00:17:10 +0200

    Create Page.java

---
+++ 25.05.2019/zadanie 5/Page.java
@@ -0,0 +1,10 @@
+/**
+ * @author Rafał Sochacki s20047
+ * @author Damian Egert s19766
+ */
+public class Page implements Turner {
+    @Override
+    public void turn() {
+        System.out.println("Going to the next page");
+    }
+}
