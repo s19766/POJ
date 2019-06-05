9dd430d2ff3b503acc2d834064f95de46be0dd3d
s19766 <48855467+s19766@users.noreply.github.com>
2019-06-06 00:16:28 +0200

    Create Grass.java

---
+++ 25.05.2019/zadanie 5/Grass.java
@@ -0,0 +1,10 @@
+/**
+ * @author Rafał Sochacki s20047
+ * @author Damian Egert s19766
+ */
+public class Grass implements Turner {
+    @Override
+    public void turn() {
+        System.out.println("swinging");
+    }
+}
