8e598fd4020abd96c06d59f89c69ed13dbfa7035
s19766 <48855467+s19766@users.noreply.github.com>
2019-06-06 00:06:03 +0200

    Create Movable.java

---
+++ 25.05.2019/zadanie 1/Movable.java
@@ -0,0 +1,14 @@
+/**
+ * @author Rafał Sochacki s20047
+ * @author Damian Egert s19766
+ */
+public interface Movable {
+
+    void moveUp();
+
+    void moveDown();
+
+    void moveLeft();
+
+    void moveRight();
+}
