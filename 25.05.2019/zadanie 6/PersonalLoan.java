070796c73a84b8aa6ae3c6886e501122f8984577
s19766 <48855467+s19766@users.noreply.github.com>
2019-06-13 20:42:29 +0200

    Update PersonalLoan.java

--- 25.05.2019/zadanie 6/PersonalLoan.java
+++ 25.05.2019/zadanie 6/PersonalLoan.java
@@ -1,3 +1,5 @@
+// Praca w parach: Rafał Sochacki s20047, Damian Egert s19766
+
 public class PersonalLoan extends Loan {
 
     PersonalLoan(String loanNum, String lastName, double loanAmount, int term, double primeIntRate) {
