71ed935a6823544dab70f7c74cc4b14337df8091
s19766 <48855467+s19766@users.noreply.github.com>
2019-06-14 19:16:43 +0200

    Update UsedCar.java

--- 25.05.2019/zadanie 8/UsedCar.java
+++ 25.05.2019/zadanie 8/UsedCar.java
@@ -9,71 +9,37 @@ public class UsedCar {
     public UsedCar(int vinNumber, String make, int year, int mileage, double price) throws UsedCarException{
 
         //not 4 digits
-        setVinNumber(vinNumber);
         if(vinNumber < 1000 || vinNumber > 9999)
-            throw(new UsedCarException());
+            throw new UsedCarException("Error: VIN is not 4 digits");
 
-        setMake(make);
         if(!("Ford".equals(make) ||
                 "Honda".equals(make) ||
                 "Toyota".equals(make) ||
                 "Chrysler".equals(make) ||
                 "Other".equals(make)))
-            throw(new UsedCarException());
+            throw new UsedCarException("Error: Make");
 
         //between 1997 and 2017
-        setYear(year);
         if(year < 1997 || year > 2017)
-            throw(new UsedCarException());
+            throw new UsedCarException("Error: Year");
 
         //not negative
-        setMileage(mileage);
         if(mileage < 0)
-            throw(new UsedCarException());
+            throw new UsedCarException("Error: Mileage must be > 0");
 
         //not negative
-        setPrice(price);
         if(price < 0)
-            throw(new UsedCarException());
-    }
+            throw new UsedCarException("Error: Price must be > 0");
 
-    public void setVinNumber(int vinNumber){
         this.vinNumber = vinNumber;
-    }
-
-    public void setMake(String make){
         this.make = make;
-    }
-
-    public void setYear(int year){
         this.year = year;
-    }
-
-    public void setMileage(int mileage){
         this.mileage = mileage;
-    }
-
-    public void setPrice(double price){
         this.price = price;
     }
 
-    public int getVinNumber(){
-        return vinNumber;
-    }
-
-    public String getMake(){
-        return make;
-    }
-
-    public int getYear(){
-        return year;
-    }
-
-    public int getMileage(){
-        return mileage;
-    }
-
-    public double getPrice(){
-        return price;
+    @Override
+    public String toString() {
+        return vinNumber + "; " + make + "; " + year + "; " + mileage + "; " + price;
     }
 }
